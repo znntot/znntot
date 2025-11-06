8846d00513c3708cfea88464206f7e9ad1a4a259
znntot <almokashfiahmed9@gmail.com>
2025-11-06 13:24:12 +0200

    Create BitQuark.java

---
+++ BitQuark.java
@@ -0,0 +1,13 @@
+public class BitQuark {
+		public int bit; // 0 or 1
+		public String color; // Red, Green, Blue
+		public String charge; // +, -, 0
+		public String spin; // up, down
+
+		public BitQuark(int bit, String color, String charge, String spin) {
+			this.bit = bit;
+			this.color = color;
+			this.charge = charge;
+			this.spin = spin;
+		}
+	}
